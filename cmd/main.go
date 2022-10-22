9e2f770670e2aa5f571c3b87bf7216f1a2f9f32e
Arcady1 <insite1029@gmail.com>
2022-10-23 01:45:45 +0300

    v1

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,63 @@
 package main
 
-import "os"
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/Arcady1/go-rest-api/pkg/handlers"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+type App struct {
+	Router *mux.Router
+	DB     *sql.DB
+}
+
+func (a *App) InitDB(user, password, dbname string) {
+	dbInfo := fmt.Sprintf("user=%s password=%s dbname=%s", user, password, dbname)
+
+	database, err := sql.Open("postgres", dbInfo)
+
+	if err != nil {
+		err = errors.New("Error: connecting to the Database")
+		log.Fatal(err)
+	} else {
+		a.DB = database
+	}
+}
+
+func (a *App) InitRoutes() {
+	a.Router = mux.NewRouter()
+
+	a.Router.HandleFunc("/api/v1/user/balance", handlers.GetUserBalance).Methods("GET")
+}
+
+func (a *App) Run(host, port string) {
+	listenAdress := fmt.Sprintf("%s:%s", host, port)
+
+	fmt.Printf("Started on: http://%s", listenAdress)
+
+	http.ListenAndServe(listenAdress, a.Router)
+
+}
 
 func main() {
 	a := App{}
 
+	// Load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error: loading .env file")
+	}
+
+	// Inits
 	a.InitDB(
 		os.Getenv("APP_DB_USERNAME"),
 		os.Getenv("APP_DB_PASSWORD"),
--- go.mod
+++ go.mod
@@ -1,8 +1,9 @@
-module go-rest-api
+module github.com/Arcady1/go-rest-api
 
 go 1.19
 
 require (
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/lib/pq v1.10.7 // indirect
 )
